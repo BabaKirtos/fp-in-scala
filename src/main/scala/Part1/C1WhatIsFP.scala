6e48d46e80ed86b7efdaaf001e8c31eb7eb8f640
Abhishek Roy <abhiroy1993@gmail.com>
2025-02-27 19:42:48 +0530

    buyCoffeesFunctional updated

--- src/main/scala/Part1/C1WhatIsFP.scala
+++ src/main/scala/Part1/C1WhatIsFP.scala
@@ -1,17 +1,29 @@
 package Part1
 
+import scala.annotation.tailrec
+
 object C1WhatIsFP extends App {
 
   trait Payments {
-    def charge(cc: CreditCard, amount: Int): String = "Success"
+    // We are forced to create an interface/trait of Payments for testing
+    // Whereas a concrete class would have been enough
+    // This introduces additional unwanted complexity
+    def charge(cc: CreditCard, amount: Int): Unit =
+      println(s"Successfully charged credit card ${cc.number} for $$$amount")
   }
 
-  class CreditCard {
-    def charge(price: Int): String = "Success"
+  class CreditCard(val number: Int) {
+    def charge(price: Int): Unit =
+      println(s"Successfully charged credit card $number for $$$price")
   }
 
   class Coffee {
     def price: Int = 10
+  }
+
+  class Charge(val cc: CreditCard, val price: Int) {
+    // We won't be calling the below method as it is a side effect
+    def executeCharge(): Unit = cc.charge(price)
   }
 
   class Cafe {
@@ -31,8 +43,31 @@ object C1WhatIsFP extends App {
       cup
     }
 
-    // How do we create a purely functional method
-    // We need to get away the side effects
-//    def buyCoffeeFunctional()
+    // How do we create a purely functional method?
+    // We need to get away with the side effects
+    // We can return Coffee along with Charge
+    def buyCoffeesFunctional(cc: CreditCard, numCups: Int = 0): (List[Coffee], Charge) = {
+      @tailrec
+      def cups(counter: Int, acc: List[Coffee] = Nil): List[Coffee] = {
+        if (counter == 0) acc
+        else cups(counter - 1, List(new Coffee) ++ acc)
+      }
+
+      val totalCoffees: List[Coffee] = cups(numCups)
+      val totalPrice = coalesce(totalCoffees)
+      (totalCoffees, new Charge(cc, totalPrice))
+    }
+
+    def coalesce(numCoffees: List[Coffee], acc: Int = 0): Int = {
+      if (numCoffees == Nil) acc
+      else coalesce(numCoffees.tail, acc + numCoffees.head.price)
+    }
   }
+
+  val testOrder = new Cafe
+  testOrder.buyCoffeeOld(new CreditCard(123))
+  testOrder.buyCoffeeNew(new CreditCard(456), new Payments {})
+  val (coffees, charge) = testOrder.buyCoffeesFunctional(new CreditCard(789), 3)
+  println(coffees)
+  println(charge.price)
 }
